5b5bf275a4baafb93e9b527aba21f3ce4f3db396
ShadowWalkerfb <shadowwalkerag@protonmail.com>
2023-09-18 02:07:27 +0000

    app_init

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Shadow Walker\Documents\GitHub\Medical_Records_Management-Project\.venv
+set VIRTUAL_ENV=C:\Users\sqado world\Documents\GitHub\Medical_Records_Management-Project\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
